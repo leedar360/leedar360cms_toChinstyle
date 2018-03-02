5d3210aaa13a58bc49fcb570053543ca10823fcb
Zhengfei Kang <348999853@qq.com>
2018-03-03 00:04:17 +0800

    动态播报

--- common/incfiles/page.inc.php
+++ common/incfiles/page.inc.php
@@ -1,12 +1,8 @@
 <?php
 require_once('const.inc.php');
 spl_autoload_register(function($argClass){
-
-  //print("The next line generates an error.<br>");
-
   $class = $argClass;
- // if (substr($class, 0, 4) == 'jtbc')
-if (substr($class, 0, 4) == 'jtbc')
+  if (substr($class, 0, 4) == 'jtbc')
   {
     $file = __DIR__ . '/lib/' . str_replace('\\', '/', $class) . '.inc.php';
     if (is_file($file)) require_once($file);
