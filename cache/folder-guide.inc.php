d16db7268208cb709d1a7dc04d2e3c5be3350cbe
leedarKen <348999853@qq.com>
2018-03-05 23:50:39 +0800

    Dev/ken/004 (#4)

--- cache/folder-guide.inc.php
+++ cache/folder-guide.inc.php
@@ -1,3 +1,3 @@
 <?php
-$GLOBALS['cache-folder-guide'] = array('0' => '1520205368','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|aboutus|+|case|+|team|+|career|+|news|+|contactus|+|consult|+|hotact|+|media|+|relation_resource|+|about_us|+|big_event|+|dongtaibobao|+|',);
+$GLOBALS['cache-folder-guide'] = array('0' => '1520264965','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
 ?>
