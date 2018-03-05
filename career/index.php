db359b2de9aca422c4db43293f073435e1ab83ec
Zhengfei Kang <348999853@qq.com>
2018-03-05 23:52:16 +0800

    add

--- career/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('content-type: text/html; charset=utf-8');
-require_once('../common/incfiles/page.inc.php');
-require_once('common/incfiles/index.inc.php');
-echo jtbc\ui::getResult();
-?>
