9436dfcb400c94969921a66c8c9ca5b9dc66c871
Zhengfei Kang <348999853@qq.com>
2018-03-07 01:02:10 +0800

    bugs

--- cache/folder-guide-category.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS['cache-folder-guide-category'] = array('0' => '1520202454','1' => 'universal|+|case|+|news|+|hotact|+|media|+|shufa|+|shici|+|guoxue|+|relation_resource|+|dongtaibobao|+|',);
-?>
--- cache/folder-guide.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$GLOBALS['cache-folder-guide'] = array('0' => '1520264965','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
-?>
