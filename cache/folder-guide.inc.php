0ee032fb61bab3d4f865a05b3b8ce2d7b6d01ed4
Zhengfei Kang <348999853@qq.com>
2018-03-12 23:41:50 +0800

    1. index meida, 2, console login page title

--- cache/folder-guide.inc.php
+++ cache/folder-guide.inc.php
@@ -1,11 +1,3 @@
 <?php
-<<<<<<< Updated upstream
-$GLOBALS['cache-folder-guide'] = array('0' => '1520549829','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
-=======
-<<<<<<< Updated upstream
-$GLOBALS['cache-folder-guide'] = array('0' => '1520442729','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
-=======
-$GLOBALS['cache-folder-guide'] = array('0' => '1520808136','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+$GLOBALS['cache-folder-guide'] = array('0' => '1520868683','1' => 'console|+|console/cache|+|console/log|+|console/xml|+|console/file|+|console/account|+|console/role|+|universal|+|universal/material|+|universal/upload|+|universal/category|+|universal/link|+|team|+|contactus|+|consult|+|about_us|+|big_event|+|hotact|+|media|+|relation_resource|+|dongtaibobao|+|',);
 ?>
